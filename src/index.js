8eec1fc76cdcf300acc6e144fdbf11f3645ba2d0
Atlas <99806895+AtlasMountains@users.noreply.github.com>
2022-10-29 11:04:19 +0200

    passing props to component

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 class Square extends React.Component {
   render() {
-    return <button className="square">{/* TODO */}</button>;
+    return <button className="square">{this.props.value}</button>;
   }
 }
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
   render() {
